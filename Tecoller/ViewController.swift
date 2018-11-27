fc23e06d5d6c0283a022f76f399592ebc1f7014c
DaikiShiwa <hssk4832>
2018-11-28 00:46:51 +0900

    Initial Commit

---
+++ Tecoller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tecoller
+//
+//  Created by 志波大輝 on 2018/11/28.
+//  Copyright © 2018 志波大輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
